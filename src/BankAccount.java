5b6de37fc67cebb9f361d7bb0106da84b8196b3e
Allan Branson <bransonaallan@gmail.com>
2024-03-19 01:24:27 +0300

    Adding a menu and a loan feature to the bank account

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,3 +1,5 @@
+import javax.sound.midi.Soundbank;
+
 public class BankAccount {
     //encapsulation -> binding methods&attributes in class
     //attributes -> instance variables
@@ -7,23 +9,26 @@ public class BankAccount {
     private String accountNumber;
     //account balance
     private int accountBalance; //inbank -> set to zero unless you deposit
+    private int loanLimit; 
 
     //default constructor
 //    public BankAccount(){
 //
 //    }
-    //parametirized const
+    //parametirized constuctor 
     public BankAccount(String name, String accountNumber){//parameters
         //this keyword -> use to refer to the current instance -parameter name equals the instance variable name
-        nameOfHolder = name;
+        this.nameOfHolder = name;
         this.accountNumber = accountNumber;
         this.accountBalance = 0;
+        this.loanLimit = 0;
+
     }
 
     //when you use private you need getters and setters
 
     //method to deposit money
-    public void depositMoney(int depositAmount){
+    public void depositMoney(double depositAmount){
 
         //account balance set to 0
         //increment the account balance with the deposit
@@ -33,6 +38,7 @@ public class BankAccount {
         }
         else {
             accountBalance +=depositAmount;
+            CalculateLoanLimit();
             System.out.println("Successfully deposited " + depositAmount);
             System.out.println("Your new balance is " + accountBalance);
         }
@@ -50,11 +56,52 @@ public class BankAccount {
                 accountBalance -= withdrawAmount;
                 System.out.println("Successfully withdrawn " + withdrawAmount);
                 System.out.println("Your new balance is " + accountBalance);
+                CalculateLoanLimit();
             }else{
                 System.out.println("Insufficient funds");
             }
         }
     }
+
+    public void CalculateLoanLimit() {
+        //creating a loan limit method to based on account balance 
+
+        if  (accountBalance < 0) {
+            System.out.println("You do not qualify for a loan");
+        }else if (accountBalance > 0 && accountBalance <= 500 ) {
+            loanLimit = accountBalance * 2;
+        } else if (accountBalance > 501 && accountBalance <= 1000) {
+            loanLimit = accountBalance * 3;
+        } else if (accountBalance >= 1001 && accountBalance <= 1500) {
+            loanLimit = accountBalance* 4;
+        }else {
+            this.loanLimit = accountBalance* 5;
+        }
+
+    }
+
+    public void requestLoan (int loanAmount) {
+        
+        //check if user has qualified for a loan 
+         if (loanAmount > loanLimit)  {
+            System.out.println("You are not eligible for a loan of: " + loanAmount);
+        } else{
+            accountBalance =  accountBalance + loanAmount;
+            System.out.println("You have been successfully granted a loan of : " + loanAmount );
+            System.out.println("Your new account balance is " + accountBalance);
+        }
+
+    }
+
+    public int getLoanLimit(){
+        return loanLimit;
+    }
+
+    public void  setLoanLimit (int loanLimit) {
+        this.loanLimit = loanLimit;
+    }
+
+
     public String getNameOfHolder() {
         return nameOfHolder;
     }
@@ -67,6 +114,7 @@ public class BankAccount {
         return accountNumber;
     }
 
+    //modify the account number to be assigned randomly
     public void setAccountNumber(String accountNumber) {
         this.accountNumber = accountNumber;
     }
@@ -78,6 +126,7 @@ public class BankAccount {
     public void setAccountBalance(int accountBalance) {
         this.accountBalance = accountBalance;
     }
+
     //toSTring -> modofied to print the class details depending on the varibales
 
     @Override
@@ -85,7 +134,9 @@ public class BankAccount {
         return "BankAccount{" +
                 "nameOfHolder='" + nameOfHolder + '\'' +
                 ", accountNumber='" + accountNumber + '\'' +
-                ", accountBalance=" + accountBalance +
+                ", loanLimit Amount='" + loanLimit + '\'' +
+                ", current accountBalance=" + accountBalance +
                 '}';
     }
+    
 }
--- src/BankMain.java
+++ src/BankMain.java
@@ -1,24 +1,106 @@
+import java.util.Scanner;
+
 public class BankMain {
 
     public static void main(String[] args) {
 
         //create our object -> real bank account
-        //className referenceVariable new(creates the obj) constructor()
-        //same name as the class
-        BankAccount allan = new BankAccount("Allan", "e02301");
-        //get
-//        String name = allan.getNameOfHolder();
-//        System.out.println("Your name is "+ name);
-//        //set balance
-//        allan.setAccountBalance(40);
-        allan.depositMoney(90);
-        allan.withdrawMoney(50);
-        allan.withdrawMoney(100);
-//
-        //ask for user details
-        //create an array of users
-        //create a method to the print user info
-        //menu of o
+        //className referenceVariable new(creates the obj) constructor() which is same name as the class
+        BankAccount branson = new BankAccount("Branson", "e02301");
+        
+        //get the name of the created account
+        String name = branson.getNameOfHolder();
+        System.out.println(" _________________________________________");
+        System.out.println("| Welcome to the Wizards Bank of Gringotts|");
+        System.out.println("|_________________________________________|");
+        System.out.println();
+        System.out.println("Your name is: " + name);
+        String accountNumber = branson.getAccountNumber();
+        System.out.println("Your Account Number is:  " + accountNumber);
 
+        //Ask user to deposit money 
+        Scanner scan = new Scanner(System.in);
+
+        //Show the User the Menu
+        System.out.println("_________________________________________");
+        System.out.println("Please select one of the following options: ");
+        int menu;
+        do {
+            System.out.println("\nMenu: ");
+            System.out.println("1. Deposit Money ");
+            System.out.println("2. Withdraw cash");
+            System.out.println("3. Check your balance");
+            System.out.println("4. Check your loan limit");
+            System.out.println("5. Request for a loan");
+            System.out.println("6. Exit ");
+            menu = scan.nextInt();
+
+            switch (menu) {
+                case 1:
+                    System.out.println("|_________________________________________|");
+                    System.out.println("Enter the amount to deposit into your account: ");
+                    double depositAmount= scan.nextDouble();
+                    branson.depositMoney(depositAmount);
+                    System.out.println("You have deposited an amount of : " + depositAmount + 
+                        " .Your new account balance is " + branson.getAccountBalance());
+                    break;
+
+                 case 2 :
+                    System.out.println("|_________________________________________|"); 
+                    System.out.println("Enter the amount to withdraw from your account: ");
+                    int withdrawAmount = scan.nextInt();
+                    branson.withdrawMoney(withdrawAmount);
+                    System.out.println("You have successfully withdrawn: " + withdrawAmount +
+                    " .Your new Account balance is " + branson.getAccountBalance());
+                    break;
+
+                 case 3 : 
+                    System.out.println("|_________________________________________|");
+                    System.out.println("Your account balance is: " + 
+                    branson.getAccountBalance());
+                    break;
+
+                 case 4 : 
+                    System.out.println("|_________________________________________|");
+                    System.out.println("Your loan limit is " + branson.getLoanLimit());
+                    break;
+
+                 case 5 :
+                    System.out.println("|_________________________________________|");
+                    System.out.println("Enter an amount to receive a  loan: ");
+                    int loanAmount = scan.nextInt();
+                    branson.requestLoan(loanAmount);
+                    System.out.println("Your account has been debited with: " + loanAmount
+                    + ".Your current account balance is " + branson.getAccountBalance());
+                    break;
+
+                 case 6 :
+                     System.out.println("|_________________________________________|");
+                    System.out.println(" Exiting the account Menu");
+                    break;
+
+                 default:
+                    System.out.println("Invalid Choice , please try again");
+                    System.out.println("|_________________________________________|");
+
+                } 
+        } while (menu != 6);
+        scan.close();
+
+        //    allan.setAccountBalance(40);
+                //     allan.depositMoney(90);
+                //     allan.withdrawMoney(50);
+                //     allan.withdrawMoney(100);
+
+                    //ToDO
+                    //ask for user details
+                    //create an array of users
+                    //create a method to the print user info
+                    //menu of of items
+
+                    //Take user Input
+                    
+
+            
     }
 }
